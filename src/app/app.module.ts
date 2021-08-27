88e22a74883f00654d4238f3a850e981821c6c99
DBR99 <dayayisramirez01@gmail.com>
2021-08-27 03:57:32 -0500

    routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,34 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {BrowserModule} from '@angular/platform-browser';
+import {LOCALE_ID, NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {AppRoutingModule} from './app-routing.module';
+import {RouterModule} from '@angular/router';
+
+import {PrimengModule} from './shared/primeng/primeng.module';
+import {SharedModule} from './shared/shared.module';
+import {AppComponent} from './app.component';
+import {LayoutComponent} from './layout/layout.component';
+import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
+import {ReactiveFormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LayoutComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    RouterModule,
+    HttpClientModule,
+    PrimengModule,
+    SharedModule,
+    ReactiveFormsModule
   ],
-  providers: [],
+  providers: [{provide: LOCALE_ID, useValue: 'es'}],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
